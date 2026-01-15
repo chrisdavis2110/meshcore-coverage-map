7365516678b846ed9622be7ffedca4502c2ac12a
chrisdavis2110 <chrisisdavis@gmail.com>
2026-01-15 14:03:08 -0800

    changed tile refresh to 3

--- server/public/content/wardrive.js
+++ server/public/content/wardrive.js
@@ -60,7 +60,7 @@ const ignoredRepeaterBtn = $("ignoredRepeaterBtn");
 const wardriveChannelHash = parseInt("e0", 16);
 const wardriveChannelKey = BufferUtils.hexToBytes("4076c315c1ef385fa93f066027320fe5");
 const wardriveChannelName = "#wardrive";
-const refreshTileAge = 1; // Tiles older than this (days) will get pinged again.
+const refreshTileAge = 3; // Tiles older than this (days) will get pinged again.
 
 // --- Global Init ---
 const utf8decoder = new TextDecoder(); // default 'utf-8'
